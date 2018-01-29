e68b5c1746b086ff6994c67e74a6903495b1a5fd
hades_zhao <hades_zhao@126.com>
2018-01-29 14:40:37 +0800

    m finished

--- rmon/models.py
+++ rmon/models.py
@@ -48,3 +48,45 @@ class Server(db.Model):
     @property
     def redis(self):
         return StrictRedis(host=self.host,port=self.port,password=self.password)
+
+from marshmallow import (Schema,fields,validate,post_load,validates_schema,ValidationError)
+
+class ServerSchema(Schema):
+    """Redis server records 
+    """
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True,validate=validate.Length(2,64))
+    description = fields.String(validate=validate.Length(0,512))
+    host = fields.String(required=True,validate=validate.Regexp(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'))
+    port = fields.Integer(validate=validate.Range(1024,65535))
+    password = fields.String()
+    updated_at = fields.DateTime(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+
+    @validates_schema
+    def validate_schema(self,data):
+        """test if the name redis server
+        """
+        if 'port' not in data:
+            data['port'] = 6379
+        instance = self.context.get('instance',None)
+        server = Server.query.filter_by(name=data['name']).first()
+        if server is None:
+            return 
+        if instance is not None and server != instance:
+            raise ValidationError('Redis server already exist','name')
+        if instance is None and server:
+            raise validationError('Redis server already exist','name')
+
+        @post_load
+        def create_or_update(self,data):
+            """data loading server auto_create server object
+            """
+            instance = self.context.get('instance',None)
+            #create redis server
+            if instance is None:
+                return Server(**data)
+            for key in data:
+                setattr(instance,key,data[key])
+            return instance
+
